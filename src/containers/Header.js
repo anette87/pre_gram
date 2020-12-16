b7f001e921523a330853a0ee48a52c748a7a8351
anette87 <anettem.rivera@gmail.com>
2020-12-15 16:41:49 -0800

    Post text center and UserInfo Component

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -15,3 +15,4 @@ class Header extends React.Component {
 }
 
 export default Header;
+
---
+++ src/containers/UserInfo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function UserInfo() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UserInfo
+
