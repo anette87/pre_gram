899f4759cc5fd400b80dfdb431366d58a293cd6b
anette87 <anettem.rivera@gmail.com>
2020-12-16 21:10:16 -0800

    CSS Username and Avatar

--- src/containers/UserInfo.js
+++ src/containers/UserInfo.js
@@ -5,7 +5,8 @@ import Avatar from "@material-ui/core/Avatar";
 function UserInfo() {
     return (
         <div className= "avatar">
-            <Avatar />
+            <Avatar className="post_avatar" alt='username'src="/static/images/avatar/1.jpg" />
+            <h3>Username</h3>
             
         </div>
     )
