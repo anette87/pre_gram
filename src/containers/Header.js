dd1147517a2319ecf5c66181856636e425ceec0f
anette87 <anettem.rivera@gmail.com>
2020-12-22 07:12:40 -0800

    NavBar

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { MenuItems } from './MenuItems';
 
 
 class Header extends React.Component {
     render(){
         return(
             <div className= "app_header">
-                <p>Header</p>
+                <nav className="NavbarItems">
+                    <h1 className="navbar-logo">PreGram</h1> 
+                <div className="menu-icon">
+                </div>  
+                <ul>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                    {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>   
+                </nav>     
             </div>
             
         )
---
+++ src/containers/MenuItems.js
@@ -0,0 +1,29 @@
+export const MenuItems = [
+    {
+       title: 'Home',
+       url: '#', //link
+       cName: 'nav-links'
+    },
+    {
+        title: 'Services',
+        url: '#', //link
+        cName: 'nav-links'
+     },
+     {
+        title: 'Products',
+        url: '#', //link
+        cName: 'nav-links'
+     },
+     {
+        title: 'Contant Us',
+        url: '#', //link
+        cName: 'nav-links'
+     },
+     {
+        title: 'Sign Up',
+        url: '#', //link
+        cName: 'nav-links'
+     }
+
+]
+
