476648bef49e352451d73f7e7e5ca3309a0aa04b
anette87 <anettem.rivera@gmail.com>
2021-01-06 20:41:09 -0800

    EditPost Container

---
+++ src/containers/EditPost.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function EditPost() {
+    return (
+        <div>
+            <p>Hello</p>   
+        </div>
+    )
+}
+
+export default EditPost
+
--- src/containers/PostInfo.js
+++ src/containers/PostInfo.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import EditPost from './EditPost';
 
 
 class PostInfo extends React.Component {
+
+    state = {
+        editButtonClicked: false
+    }
+
+    handleEditClick = () => {
+        this.setState({
+            editButtonClicked: true
+        })   
+    }
+
     render(){
-        {console.log(this.props)}
+        {console.log(this.state.editButtonClicked)}
         return(
             <div>
-                <p>Date: {this.props.date}</p>
-                <p>Caption: {this.props.caption}</p>
-                <p>Hashtags: {this.props.hashtags} </p>
+                {(this.state.editButtonClicked === false)
+                ?
+                <div>
+                    <p>Date: {this.props.date}</p>
+                    <p>Caption: {this.props.caption}</p>
+                    <p>Hashtags: {this.props.hashtags} </p>
+                    <br></br><input onClick={this.handleEditClick}type="submit" value="Edit" />
+                </div>
+                :
+                <EditPost date={this.props.date} caption={this.props.caption} hashtags={this.props.hashtags} />
+                }
+                
             </div>
             
         )
