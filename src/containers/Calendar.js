ccf44fa9732b848872621b28d23232f8e5e6723a
anette87 <anettem.rivera@gmail.com>
2021-01-21 19:25:46 -0800

    Remove @syncfusion/ej2-react-schedule

--- src/containers/Calendar.js
+++ src/containers/Calendar.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 export default function Calendar() {
     return (
-        <div>
-            <p>Calendar</p>
-        </div>
+           <div>
+               <p></p>
+           </div>
+
+        
     )
 }
+
+
