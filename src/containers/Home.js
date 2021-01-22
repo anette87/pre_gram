df3e659178079ef1658716a8ececfa2870cbf9bb
anette87 <anettem.rivera@gmail.com>
2021-01-21 19:49:29 -0800

    Home: Loggin Form

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 export default function Calendar() {
-    return (
-           <div>
-               <p>Home</p>
-           </div>
 
-        
-    )
+    return (
+        <div>
+            <form id="login-form" class="login-form">
+                <label for="user-id" >Email:</label>
+                <input type="email" name="user-id" id="partner-user-ID"></input>
+                <label for="user-password" >Password:</label>
+                <input type="password" name="user-password" id="partner-user-secret"></input>
+                <input type="submit" id="submit-button" value="Submit"></input>   
+            </form>   
+        </div> 
+        )
 }
+
+
