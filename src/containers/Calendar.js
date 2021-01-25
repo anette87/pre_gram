84dba3c6a1be86fc635bf0db9a1c56a91043c8af
anette87 <anettem.rivera@gmail.com>
2021-01-24 21:42:31 -0800

    Form Calendar Days

--- src/containers/Calendar.js
+++ src/containers/Calendar.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import moment from "moment";
 
 export default function Calendar() {
 
-    return (
-           <div>
-               <p></p>
-           </div>
+    const value = moment();
+    const startDay = value.clone().startOf("month").startOf("week"); 
+    const endDay = value.clone().endOf("month").endOf("week");
+    const day = startDay.clone().subtract(1, "day")
+    const calendar = []
+    
+    while(day.isBefore(endDay, "day")){
+        calendar.push(
+            Array(7).fill(0).map(() => day.add(1, "day").clone())
+        )
+    }
 
-        
-    )
+
+    return (
+           <div className="calendar">
+               {calendar.map((week) => (
+                   <div>
+                       {week.map((day) => (
+                           <div>{day.format("D").toString()}</div>
+                        ))}
+                    </div>
+                ))}
+            </div>        
+        )
 }
 
 
