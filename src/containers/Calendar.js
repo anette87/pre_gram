a22ff9e647ae5734d4863de60dae3736f592897c
anette87 <anettem.rivera@gmail.com>
2021-01-25 20:43:31 -0800

    Header

--- src/containers/Calendar.js
+++ src/containers/Calendar.js
@@ -20,19 +20,72 @@ export default function Calendar() {
         }
         setCalendar(a);
     },[value])
+
+    function isSelected(day) {
+        return value.isSame(day, "day")
+    }
+
+    function beforeToday(day) {
+        return day.isBefore(new Date(), "day")
+    }
+
+    function isToday(day){
+        return day.isSame(new Date(), "day")
+    }
+
+    function dayStyles(day){
+        if (beforeToday(day)) return "before"
+        if (isSelected(day)) return "selected"
+        if (isToday(day)) return "today"
+        return ""
+    }
+
+    function currMonthName(){
+        return value.format("MMMM")
+    }
     
+    function currYear(){
+        return value.format("YYYY")
+    }
+
+    function prevMonth(){
+        return value.clone().subtract(1, "month")
+    }
+
+    function nextMonth(){
+        return value.clone().add(1, "month")
+    }
+
+    function thisMonth(){
+        return value.isSame(new Date(), "month")
+    }
+
     return (
-           <table className="calendar">
-               {calendar.map((week) => (
-                   <div>
-                       {week.map((day) => (
-                           <td className="day" onClick={() => setValue(day)}>
-                               <div className={value.isSame(day, "day") ? "selected" : ""}>{day.format("D").toString()}</div>
-                            </td>
+            <div className="calendar">
+                <div className="header">
+                    <div className="previous" onClick={() => !thisMonth() &&setValue(prevMonth())}>{!thisMonth() ? String.fromCharCode(171) : null }</div>
+                    <div className= "current">{currMonthName()} {currYear()}</div>
+                    <div className= "next" onClick={() => setValue(nextMonth())}>{String.fromCharCode(187)}</div>
+                </div>
+                <div className="body">
+                    <div className="day-names">
+                        {["s","m","t","w","t","f","s"].map((d) => (
+                            <div className="week">{d}</div>
                         ))}
                     </div>
-                ))}
-            </table>        
+                    <table>
+                        {calendar.map((week) => (
+                            <div>
+                                {week.map((day) => (
+                                    <td className="day" onClick={() => !beforeToday(day) && setValue(day)}>
+                                    <div className={dayStyles(day)}>{day.format("D").toString()}</div>
+                                    </td>
+                                ))}
+                            </div>
+                        ))}
+                    </table> 
+                </div>     
+           </div>  
         )
 }
 
